594b5b68742f811f984266789ef0c11e61ba0ac3
Đặng Trương Minh Trí <87639905+seeyounxttime@users.noreply.github.com>
2023-08-20 09:32:49 +0700

    update jobcard

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,128 +1,77 @@
-import React, { useContext } from "react";
-import {
-  Button,
-  Box,
-  Card,
-  CardContent,
-  CardActions,
-  Typography,
-  Divider,
-  Chip,
-} from "@mui/material";
-import { styled } from "@mui/material/styles";
+import React from "react";
+import { useContext } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+import AuthContext from "../auth/AuthContext";
+import { useNavigate } from "react-router-dom";
 import { useLocation, Link } from "react-router-dom";
-import { JobPostingsContext } from "../../App";
+import SkillsPaper from "./SkillsPaper";
+import { styled } from "@mui/material/styles";
 
-function JobCard({job}) {
-  const contextProps = useContext(JobPostingsContext);
+const CardStyle = styled(Card)(({ theme }) => ({
+  boxShadow: "none",
+  border: "1px solid black",
+  width: "100%",
+  maxWidth: "350px",
+  minWidth: "270px",
+  height: "320px",
+  margin: "auto",
+  backgroundColor: theme.palette.primary.light,
+}));
 
-  const location = useLocation();
-
-  const limitedSkills = () => {
-    if (job.skills.length > 4) {
-      return [...job.skills.slice(0, 4)];
+function JobCard({ description, skills, id, title }) {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+  let location = useLocation();
+  const handleClick = (event) => {
+    if (auth.user) {
+      navigate(`/job/${id}`);
     } else {
-      return [...job.skills.slice(0)];
+      navigate("/login");
     }
   };
-
-  const handleOpenJobDetails = () => {
-    if (!localStorage.getItem("username")) {
-      contextProps.setOpenSignIn(true);
-    } else {
-      contextProps.setOpenJobDetails(true);
-    }
-  };
-
   return (
-    <>
-      <CustomJobCard>
-        <CardContent sx={{ padding: 0 }}>
-          <Typography variant="h5" className="job-title">
-            {job.title}
+    <CardStyle ariant="outlined">
+      <Stack
+        direction="column"
+        justifyContent="space-between"
+        alignItems="center"
+        height="100%"
+        padding="5px"
+      >
+        <CardContent>
+          <Typography
+            variant="subtitle1"
+            component="div"
+            sx={{ color: (theme) => theme.palette.common.white }}
+          >
+            {title}
           </Typography>
           <Divider />
-          <SkillsContainer>
-            {limitedSkills().map((skill, index) => (
-              <Chip key={index} label={skill} />
-            ))}
-          </SkillsContainer>
-          <Typography className="job-description">{job.description}</Typography>
+          <SkillsPaper skills={skills} />
+          <Typography sx={{ color: (theme) => theme.palette.common.white }}>
+            {description}
+          </Typography>
         </CardContent>
-        <CardActions
-          sx={{
-            marginTop: "50px",
-            padding: 0,
-            display: "flex",
-            justifyContent: "center",
-          }}
+        <Button
+          variant="contained"
+          component={Link}
+          to={`/job/${id}`}
+          state={{ backgroundLocation: location }}
+          sx={{ width: "130px", backgroundColor: "#df9e0b" }}
         >
-          <Link
-            to={localStorage.getItem("username") ? `/jobs/${job.id}` : "/login"}
-            onClick={handleOpenJobDetails}
-            state={{ background: location }}
-          >
-            <JobButton>Learn More</JobButton>
-          </Link>
-        </CardActions>
-      </CustomJobCard>
-    </>
+          Learn More
+        </Button>
+        {/* <Link to={`/job/${id}`} state={{ backgroundLocation: location }}>
+            <div>Learn More</div>
+          </Link> */}
+      </Stack>
+    </CardStyle>
   );
 }
-
-const CustomJobCard = styled(Card)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.light,
-  padding: "20px",
-  width: "100%",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-  "& .MuiTypography-root": {
-    color: "#fff",
-  },
-
-  "& .MuiDivider-root": {
-    borderColor: "grey",
-  },
-  "& .job-title": {
-    textAlign: "center",
-    marginBottom: "8px",
-  },
-  "& .job-description": {
-    lineHeight: 1.5,
-    marginTop: "24px",
-  },
-  [theme.breakpoints.up("sm")]: {
-    width: "49.5%",
-  },
-  [theme.breakpoints.up("md")]: {
-    width: "33%",
-  },
-}));
-
-const SkillsContainer = styled(Box)(({ theme }) => ({
-  display: "flex",
-  flexWrap: "wrap",
-  gap: "4px",
-  marginTop: "8px",
-  "& .MuiChip-root": {
-    backgroundColor: theme.palette.error.main,
-    padding: "6px 12px",
-    height: "26px",
-    borderRadius: "20px",
-  },
-  "& .MuiChip-label": {
-    color: "#fff",
-    padding: 0,
-    fontSize: "0.9rem",
-  },
-}));
-
-const JobButton = styled(Button)(({ theme }) => ({
-  backgroundColor: theme.palette.warning.main,
-  "&:hover": {
-    backgroundColor: theme.palette.warning.main,
-  },
-}));
 
 export default JobCard;
